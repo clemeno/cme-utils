c359b7877849015bd1d975f5981f8554cc9deccc
옷 CLEm ツ <clemeno@gmail.com>
2025-04-22 16:43:14 +0200

    chore: continue to get rid of runtime external dependencies, publish 0.0.19

--- ts/number.util.ts
+++ ts/number.util.ts
@@ -16,12 +16,28 @@ export const GET_RANDOM_INT_BETWEEN = (_: { a: number, b: number }): number => {
   return Math.floor(Math.random() * Math.max(1, max - min)) + min
 }
 
-export const TO_LOCAL_NUMBER = (x: any): string => IS_NUMERIC(x) ? TO_NUMBER(x).toLocaleString(GET_LOCALE()) : ''
+export const TO_LOCAL_NUMBER = (_: {
+  x: any
+  /** @type {Luxon.Settings} */
+  Settings: any
+}): string => IS_NUMERIC(_.x) ? TO_NUMBER(_.x).toLocaleString(GET_LOCALE(_.Settings)) : ''
 
-export const TO_LOCAL_NUMBER_WITH_2_F_DIGITS = (x: any): string => {
-  return IS_NUMERIC(x) ? TO_NUMBER(x).toLocaleString(GET_LOCALE(), { minimumFractionDigits: 2, maximumFractionDigits: 2 }) : ''
-}
+export const TO_LOCAL_NUMBER_WITH_2_F_DIGITS = (_: {
+  x: any
+  /** @type {Luxon.Settings} */
+  Settings: any
+}): string => (
+  IS_NUMERIC(_.x)
+    ? TO_NUMBER(_.x).toLocaleString(GET_LOCALE(_.Settings), { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+    : ''
+)
 
-export const TO_LOCAL_NUMBER_WITH_1_F_DIGIT = (x: any): string => {
-  return IS_NUMERIC(x) ? TO_NUMBER(x).toLocaleString(GET_LOCALE(), { minimumFractionDigits: 1, maximumFractionDigits: 1 }) : ''
-}
+export const TO_LOCAL_NUMBER_WITH_1_F_DIGIT = (_: {
+  x: any
+  /** @type {Luxon.Settings} */
+  Settings: any
+}): string => (
+  IS_NUMERIC(_.x)
+    ? TO_NUMBER(_.x).toLocaleString(GET_LOCALE(_.Settings), { minimumFractionDigits: 1, maximumFractionDigits: 1 })
+    : ''
+)
