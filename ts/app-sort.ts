1b3ea59f7a519289d4c14cc65e8e9b904aa4406a
옷 CLEm ツ <clemeno@gmail.com>
2025-04-22 14:12:07 +0200

    feat: add support for AppSort, publish 0.0.18

---
+++ ts/app-sort.ts
@@ -0,0 +1,14 @@
+import { IS_SET } from 'check/is-set.util'
+
+/** Used to match the Angular `Sort` interface: `column` <-> `active` ; `order` <-> `direction` */
+export class AppSort {
+  column = ''
+  order?: 'asc' | 'desc' | ''
+  bDisabled?: boolean
+
+  constructor (_?: Partial<AppSort>) {
+    if (IS_SET(_)) {
+      Object.assign(this, _)
+    }
+  }
+}
