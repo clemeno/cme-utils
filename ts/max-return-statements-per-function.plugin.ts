a9d642c741a0e9087cd82c436a0d6dc819f38bd5
옷 CLEm ツ <clemeno@gmail.com>
2025-05-07 09:49:09 +0200

    chore: export eslint plugins, prepare 0.1.2

---
+++ ts/max-return-statements-per-function.plugin.ts
@@ -0,0 +1,7 @@
+export const maxReturnStatementsPerFunctionPlugin = {
+  rules: {
+    'max-return-statements-per-function': require('../max-return-statements-per-function.rule.cjs'),
+  },
+}
+
+export default maxReturnStatementsPerFunctionPlugin
