ac0a16d849d58007efc3896bde9b1394f8e5cd92
옷 CLEm ツ <clemeno@gmail.com>
2025-05-14 14:35:01 +0200

    chore: better specify AppFilter options, publish 0.1.19

--- ts/app-filter.ts
+++ ts/app-filter.ts
@@ -3,11 +3,11 @@ export class AppFilter {
   operator = ''
   value?: any
   options?: {
-    bDbSideToHex?: 0
-    bDbSideToUpper?: 0
-    bCsnDecLast8Bytes?: 0
-    bCsnHexLast8Bytes?: 0
-    bEnforceIntlPhone?: 0
+    bDbSideToHex?: 0 | 1
+    bDbSideToUpper?: 0 | 1
+    bCsnDecLast8Bytes?: 0 | 1
+    bCsnHexLast8Bytes?: 0 | 1
+    bEnforceIntlPhone?: 0 | 1
   }
 
   constructor (_: Partial<AppFilter>) {
