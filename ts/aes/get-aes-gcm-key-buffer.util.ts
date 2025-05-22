ce867667f8d90185e7a93193b532ae6ecc9d9045
옷 CLEm ツ <clemeno@gmail.com>
2025-05-22 18:24:10 +0200

    feat: provide node dependencies manually to build frontend projects without error or warning, publish 0.1.21

--- ts/aes/get-aes-gcm-key-buffer.util.ts
+++ ts/aes/get-aes-gcm-key-buffer.util.ts
@@ -1,6 +1,12 @@
 import { GET_FILE_FULL_TEXT_CONTENT } from '../file/get-file-full-text-content.util.js'
 
-export const GET_AES_GCM_KEY_BUFFER = async (aes128GcmKeyFile: string): Promise<Buffer> => Buffer.from(
-  await GET_FILE_FULL_TEXT_CONTENT(aes128GcmKeyFile),
+/**
+ * import { createReadStream } from 'node:fs'
+ */
+export const GET_AES_GCM_KEY_BUFFER = async (_: {
+  aes128GcmKeyFile: string
+  createReadStream: any
+}): Promise<Buffer> => Buffer.from(
+  await GET_FILE_FULL_TEXT_CONTENT({ filePath: _.aes128GcmKeyFile, createReadStream: _.createReadStream }),
   'base64'
 )
--- ts/aes/get-aes-gcm-key-sftp-buffer.util.ts
+++ ts/aes/get-aes-gcm-key-sftp-buffer.util.ts
@@ -1,6 +1,12 @@
 import { GET_FILE_FULL_TEXT_CONTENT } from '../file/get-file-full-text-content.util.js'
 
-export const GET_AES_GCM_KEY_SFTP_BUFFER = async (aes128GcmKeySftpFile: string): Promise<Buffer> => Buffer.from(
-  await GET_FILE_FULL_TEXT_CONTENT(aes128GcmKeySftpFile),
+/**
+ * import { createReadStream } from 'node:fs'
+ */
+export const GET_AES_GCM_KEY_SFTP_BUFFER = async (_: {
+  aes128GcmKeySftpFile: string
+  createReadStream: any
+}): Promise<Buffer> => Buffer.from(
+  await GET_FILE_FULL_TEXT_CONTENT({ filePath: _.aes128GcmKeySftpFile, createReadStream: _.createReadStream }),
   'base64'
 )
