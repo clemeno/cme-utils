98832e171c745ea0e00fd739deb3b2c41a54e71e
옷 CLEm ツ <clemeno@gmail.com>
2026-03-05 16:50:09 +0100

    fix: strict relative esm imports + respect isomorphism

--- ts/check/is-an-int.util.ts
+++ ts/check/is-an-int.util.ts
@@ -1,10 +1,8 @@
-import { TO_BIGINT_OR_NULL } from 'bigint/to-bigint-or-null.util.js'
+import { TO_BIGINT_OR_NULL } from '../bigint/to-bigint-or-null.util.js'
 import type { int } from '../int.js'
 import { IS_NULL } from './is-null.util.js'
 
-/**
- * Checks whether the given value is an integer (bigint, integer number, or numeric integer string).
- */
+/** Checks whether the given value is an integer (bigint, integer number, or numeric integer string). */
 export function IS_AN_INT (v: unknown): v is int {
   return !IS_NULL(TO_BIGINT_OR_NULL(v))
 }
--- ts/convert/from-any-to-buffer.util.ts
+++ ts/convert/from-any-to-buffer.util.ts
@@ -1,3 +1,5 @@
-export function FROM_ANY_TO_BUFFER (_: any): Buffer {
-  return ArrayBuffer.isView(_) ? Buffer.from(_.buffer, _.byteOffset, _.byteLength) : Buffer.from(_)
+export function FROM_ANY_TO_BUFFER <Buffer = any, TypeofBuffer = any> (_: { from: any, Buffer: TypeofBuffer }): Buffer {
+  const bView = ArrayBuffer.isView(_.from)
+
+  return bView ? (_.Buffer as any).from(_.from.buffer, _.from.byteOffset, _.from.byteLength) : (_.Buffer as any).from(_.from)
 }
